f7f1bdf8336430512dfc4168b2f0fb45d8253927
Vvvviper <xuweijie97@gmail.com>
2022-01-27 20:55:54 -0800

    particle

---
+++ Assets/_Scripts/EnvironmentBehavior/Edge.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Edge
+{
+    public Node neighbor;
+    public bool isActive;
+}
+
