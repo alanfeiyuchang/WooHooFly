22225633542cddfcef969a3f21c47a540388f85d
Gehan Yang <yanggehan2019app@outlook.com>
2022-01-29 23:17:37 -0800

    Increase the movement mode of the wall

--- Assets/_Scripts/Controller/CubeController.cs
+++ Assets/_Scripts/Controller/CubeController.cs
@@ -6,6 +6,7 @@ public class CubeController : MonoBehaviour
 {
     public int speed = 300;//翻转的速度，可以直接在unity修改
     bool isMoving = false;
+    bool onWall = false;
 
     void Update()
     {
@@ -34,27 +35,56 @@ public class CubeController : MonoBehaviour
         {
             StartCoroutine(Roll(Vector3.back));
         }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            StartCoroutine(Roll(Vector3.up));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            StartCoroutine(Roll(Vector3.down));
+        }
     }
-    void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        Debug.Log("fs"+collision.gameObject.name);
+        if(other.gameObject.tag == "wall")
+        {
+            Debug.Log("fs" + other.gameObject.tag);
+        }
+        
     }
-        IEnumerator Roll(Vector3 direction)
+    IEnumerator Roll(Vector3 direction)
     {
         isMoving = true;
-
+        Vector3 rotationPoint;
+        Vector3 rotationAxis ;
         float remainingAngle = 90;
-        Vector3 rotationPoint = transform.position + direction / 2 + Vector3.down / 2;
-        Vector3 rotationAxis = Vector3.Cross(Vector3.up, direction);
+        if (direction == Vector3.up)
+        {
+            rotationPoint = transform.position + Vector3.forward / 2 + Vector3.up / 2;
+            rotationAxis = new Vector3(1,0,0);
+        }
+        else if (direction == Vector3.down)
+        {
+            rotationPoint = transform.position + Vector3.forward / 2 + Vector3.down / 2;
+            rotationAxis = new Vector3(-1, 0, 0);
+        }
+        else
+        {
+            rotationPoint = transform.position + direction / 2 + Vector3.down / 2;
+            rotationAxis = Vector3.Cross(Vector3.up, direction);
+
+        }
+        
+        
 
         while (remainingAngle > 0)
         {
+            
             float rotationAngle = Mathf.Min(Time.deltaTime * speed, remainingAngle);
             transform.RotateAround(rotationPoint, rotationAxis, rotationAngle);
             remainingAngle -= rotationAngle;
             yield return null;
         }
-
         isMoving = false;
     }
 }
