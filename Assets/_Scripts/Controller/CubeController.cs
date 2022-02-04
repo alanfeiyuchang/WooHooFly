d639ff94a759757d3df57ec6d6612577e7a622ab
Fennggg <fengguan@usc.edu>
2022-02-03 18:57:30 -0800

    Revert "Merge branch 'main' of https://github.com/alanfeiyuchang/WooHooFly"
    This reverts commit 66904e99abc1f1a2788db11205d6dc75c77f191b, reversing
    changes made to 1a9b00d820945ceb92c88fd1790748db4959ad60.

--- Assets/_Scripts/Controller/CubeController.cs
+++ Assets/_Scripts/Controller/CubeController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class CubeController : MonoBehaviour
 {
-    public int speed = 300;
+    public int speed = 300;//��ת���ٶȣ�����ֱ����unity�޸�
     bool isMoving = false;
 
     public bool[] edgeDirection = new bool[4];
@@ -15,56 +15,6 @@ public class CubeController : MonoBehaviour
 
     void Update()
     {
-        if (isMoving)
-        {
-            return;
-        }
-
-
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if (edgeDirection[3] == true)
-            {
-                return;
-            }
-
-            StartCoroutine(Roll(Vector3.right));
-        }
-        else if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (edgeDirection[2] == true)
-            {
-                return;
-            }
-            StartCoroutine(Roll(Vector3.left));
-        }
-        else if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (edgeDirection[0] == true)
-            {
-                return;
-            }
-            StartCoroutine(Roll(Vector3.forward));
-        }
-        else if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (edgeDirection[1] == true)
-            {
-                return;
-            }
-            StartCoroutine(Roll(Vector3.back));
-        }
-        else if (Input.GetKey(KeyCode.W))
-        {
-            if (connectDirection[0] || connectDirection[2] || connectDirection[3])
-                StartCoroutine(Roll(Vector3.up));
-        }
-        // when is cube going south?
-        else if (Input.GetKey(KeyCode.S))
-        {
-            if (connectDirection[1])
-                StartCoroutine(Roll(Vector3.down));
-        }
         
     }
 
@@ -75,7 +25,52 @@ public class CubeController : MonoBehaviour
     void FixedUpdate()
     {
     
-        
+        if (isMoving)//����Ҫ��һ����ת��ɣ����ܽ�����һ����ת
+        {
+            return;
+        }
+
+        //�������õ�����������
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            if (edgeDirection[3] == true) {
+                return;
+            }
+            
+            StartCoroutine(Roll(Vector3.right));
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            if (edgeDirection[2] == true) {
+                return;
+            }
+            StartCoroutine(Roll(Vector3.left));
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            if (edgeDirection[0] == true) {
+                return;
+            }
+            StartCoroutine(Roll(Vector3.forward));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            if (edgeDirection[1] == true) {
+                return;
+            }
+            StartCoroutine(Roll(Vector3.back));
+        }
+        else if (Input.GetKey(KeyCode.W))
+        {
+            if (connectDirection[0])
+                StartCoroutine(Roll(Vector3.up)); 
+        }
+        // when is cube going south?
+        else if (Input.GetKey(KeyCode.S))
+        {
+            if (connectDirection[1])
+                StartCoroutine(Roll(Vector3.down));
+        }
     }
 
 
@@ -85,30 +80,14 @@ public class CubeController : MonoBehaviour
         Vector3 rotationPoint;
         Vector3 rotationAxis ;
         float remainingAngle = 90;
-        if (direction == Vector3.up && connectDirection[0])
+        if (direction == Vector3.up)
         {
             rotationPoint = transform.position + Vector3.forward / 2 + Vector3.up / 2;
             rotationAxis = new Vector3(1,0,0);
         }
-        else if (direction == Vector3.down && connectDirection[1] && connectDirection[0])
+        else if (direction == Vector3.down)
         {
             rotationPoint = transform.position + Vector3.forward / 2 + Vector3.down / 2;
-            rotationAxis = new Vector3(-1, 0, 0);
-        }
-        else if (direction == Vector3.down && connectDirection[1] && connectDirection[2])
-        {
-            rotationPoint = transform.position + Vector3.left / 2 + Vector3.down / 2;
-            rotationAxis = new Vector3(0, 0, -1);
-        }
-        else if (direction == Vector3.up && connectDirection[2])
-        {
-            
-            rotationPoint = transform.position + Vector3.left / 2 + Vector3.up / 2;
-            rotationAxis = new Vector3(0, 0, 1);
-        }
-        else if (direction == Vector3.up && connectDirection[3])
-        {
-            rotationPoint = transform.position + Vector3.forward / 2 + Vector3.up / 2;
             rotationAxis = new Vector3(-1, 0, 0);
         }
         else
