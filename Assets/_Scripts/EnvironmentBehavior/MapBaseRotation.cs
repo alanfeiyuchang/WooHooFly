dabaa15e37bea9ced5c151f9912302a488c241ba
stevenxmh <Xmh911111@gmail.com>
2022-02-05 19:02:34 -0800

    Added camera change
    Added camera change when rotation completes

--- Assets/_Scripts/EnvironmentBehavior/MapBaseRotation.cs
+++ Assets/_Scripts/EnvironmentBehavior/MapBaseRotation.cs
@@ -7,7 +7,9 @@ public class MapBaseRotation : MonoBehaviour
     bool isMoving = false;
     public float time = 3.0f;
     public float speed = 3.0f;
-
+    public GameObject camera1;
+    public GameObject camera2;
+    private bool camOneActive = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,10 +25,17 @@ public class MapBaseRotation : MonoBehaviour
             return;
         }
 
-        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             StartCoroutine(RotateMap(Vector3.forward));
-        }  
+            camOneActive = false;
+        }
+        else if (Input.GetKeyDown(KeyCode.Backspace))
+        {
+            StartCoroutine(RotateMap(Vector3.back));
+            camOneActive = true;
+        }
+        /*
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             StartCoroutine(RotateMap(Vector3.back));
@@ -39,6 +48,7 @@ public class MapBaseRotation : MonoBehaviour
         {
             StartCoroutine(RotateMap(Vector3.left));
         }
+        */
     }
 
     IEnumerator RotateMap(Vector3 direction)
@@ -54,6 +64,10 @@ public class MapBaseRotation : MonoBehaviour
             remainingAngle -= rotationAngle;
             yield return null;
         }
+
+        camera1.SetActive(camOneActive);
+        camera2.SetActive(!camOneActive);
+
         isMoving = false;
     }
 }
