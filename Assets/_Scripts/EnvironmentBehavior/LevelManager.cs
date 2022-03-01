1166263813b73781017919a1c5182888486cdeef
alanfeiyuchang <alanfeiyuchang@gmail.com>
2022-03-01 14:41:51 -0800

    prototype

--- Assets/_Scripts/EnvironmentBehavior/LevelManager.cs
+++ Assets/_Scripts/EnvironmentBehavior/LevelManager.cs
@@ -7,7 +7,7 @@ public class LevelManager : MonoBehaviour
     public List<GameObject> MapCubes;
     public List<GameObject> Flags;
     public GameObject PlayerCube;
-    //[HideInInspector]
+    [HideInInspector]
     public GameObject Dummy;
 
     private void Start()
