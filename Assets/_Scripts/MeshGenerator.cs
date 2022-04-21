58d085b8f5bc48b3c744ca2f15370ee8330197c1
yyycct <yyycct02@gmail.com>
2022-04-21 14:58:11 -0700

    flag

--- Assets/_Scripts/MeshGenerator.cs
+++ Assets/_Scripts/MeshGenerator.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour
 {
-    [SerializeField] public float meshSizeX;
-    [SerializeField] public float meshSizeY;
+    [SerializeField] public int vertAmountX;
+    [SerializeField] public int vertAmountY;
     [SerializeField] public int xSize;
     [SerializeField] public int ySize;
     Mesh mesh;
@@ -26,52 +26,59 @@ public class MeshGenerator : MonoBehaviour
     {
         
     }
+    private void GenerateTest()
+    {
+        verticles = new Vector3[4];
+        verticles[0] = new Vector3(0.5f, 0.5f, 0);
+        verticles[1] = new Vector3(-0.5f, 0.5f, 0);
+        verticles[2] = new Vector3(0.5f, -0.5f, 0);
+        verticles[3] = new Vector3(-0.5f, -0.5f, 0);
+        triangles = new int[6]
+        {
+            1,3,2,2,1,4
+        };
+    }
     private void GenerateFlag()
     {
-        verticles = new Vector3[(xSize + 1) * (ySize + 1)];
+        verticles = new Vector3[(vertAmountX + 1) * (vertAmountY + 1)];
         uvs = new Vector2[verticles.Length];
-        for (int i = 0, y = 0; y <= ySize; y++)
+        for (int i = 0, y = 0; y <= vertAmountY; y++)
         {
-            for (int x = 0; x <= xSize; x++)
+            for (int x = 0; x <= vertAmountX; x++)
             {
-                verticles[i] = new Vector3(x, y, 0);
-                uvs[i] = new Vector2((float)x / xSize, (float)y / ySize);
+                float vertX = x * xSize / vertAmountX;
+                float vertY = y * ySize / vertAmountY;
+                verticles[i] = new Vector3(vertX, vertY, 0);
+                uvs[i] = new Vector2((float)x / vertAmountX, (float)y / vertAmountY);
                 i++;
             }
         }
-        triangles = new int[xSize * ySize * 6];
+        triangles = new int[vertAmountX * vertAmountY * 6];
         int vert = 0;
         int tris = 0;
-        for(int y = 0; y < ySize; y++)
+        for(int y = 0; y < vertAmountY; y++)
         {
-            for(int x = 0; x < xSize; x++)
+            for(int x = 0; x < vertAmountX; x++)
             {
                 triangles[tris + 0] = vert;
-                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 1] = vert + vertAmountX + 1;
                 triangles[tris + 2] = vert + 1;
                 triangles[tris + 3] = vert + 1;
-                triangles[tris + 4] = vert + xSize + 1;
-                triangles[tris + 5] = vert + xSize + 1;
+                triangles[tris + 4] = vert + vertAmountX + 1;
+                triangles[tris + 5] = vert + vertAmountX + 2;
                 vert++;
                 tris += 6;
             }
             vert++;
         }
-        for (int i = 0, y = 0; y <= ySize; y++)
-        {
-            for (int x = 0; x <= xSize; x++)
-            {
-                verticles[i] = new Vector3(x, y, 0);
-                i++;
-            }
-        }
+ 
     }
     private void updateMesh()
     {
         mesh.Clear();
         mesh.vertices = verticles;
         mesh.triangles = triangles;
-        mesh.uv = uvs;
+        //mesh.uv = uvs;
         mesh.RecalculateNormals();
     }
 }
