0981ed81b0ad6ad443b10265ebee001bbd6a4dbb
stevenxmh <Xmh911111@gmail.com>
2022-04-25 23:39:43 -0700

    WooHoofly level

--- Assets/FinalTransition.cs
+++ Assets/FinalTransition.cs
@@ -13,7 +13,7 @@ public class FinalTransition : MonoBehaviour
     // (-0.212 132.38 87.376) (0 -46.147 -87.363)
     public float[] scales;
     private List<Vector3> startPositions = new List<Vector3>();
-    private List<Quaternion>startRotations = new List<Quaternion>();
+    private List<Quaternion> startRotations = new List<Quaternion>();
     private List<Vector3> initialSizes = new List<Vector3>();
     public float desiredDuration = 1f;
     private float elapsedTime;
@@ -21,19 +21,24 @@ public class FinalTransition : MonoBehaviour
     public bool allAtOnce;
 
     private bool transformed;
-    void TransformAllAtOnce() {
+    void TransformAllAtOnce()
+    {
 
     }
 
-     private void Update() {
-         if (Input.GetKeyDown(KeyCode.F)) {
-             if (!transformed) {
-                 Begin();
-             }
-         }
-     }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if (!transformed)
+            {
+                Begin();
+            }
+        }
+    }
 
-    public void Begin() {
+    public void Begin()
+    {
         if (endPositions == null || endPositions.Length != transform.childCount)
             return;
 
@@ -49,37 +54,15 @@ public class FinalTransition : MonoBehaviour
             {
                 child.GetChild(j).gameObject.SetActive(true);
             }
-            
-        }               
-        
+
+        }
+
 
         //
         //change top color to blue, bottom to red
-        for (int i = 0; i< transform.childCount-1; i++)
+        for (int i = 0; i < transform.childCount - 1; i++)
         {
             Transform _child = transform.GetChild(i);
-            for(int j = 0; j < _child.childCount; j++)
-            {
-                GameObject _mapCube = _child.GetChild(j).gameObject;
-                MapCubeManager _mapManager = _mapCube.GetComponent<MapCubeManager>();
-                TileColor _color;
-                if (i < 4)
-                {
-                    _color = TileColor.green;
-                }
-                else
-                {
-                    _color = TileColor.red;
-                }
-                _mapManager.changeAllTileColor(_color);
-                _mapManager.changeAllTileType(MapCubeManager.TileType.Changeable);
-
-            }
-        }
-        Transform _childY = transform.GetChild(transform.childCount-1);
-        for (int i = 0; i < _childY.childCount; i++)
-        {
-            Transform _child = _childY.GetChild(i);
             for (int j = 0; j < _child.childCount; j++)
             {
                 GameObject _mapCube = _child.GetChild(j).gameObject;
@@ -98,35 +81,53 @@ public class FinalTransition : MonoBehaviour
 
             }
         }
+        Transform _childY = transform.GetChild(transform.childCount - 1);
+        for (int i = 0; i < _childY.childCount; i++)
+        {
+            Transform _child = _childY.GetChild(i);
+            for (int j = 0; j < _child.childCount; j++)
+            {
+                GameObject _mapCube = _child.GetChild(j).gameObject;
+                MapCubeManager _mapManager = _mapCube.GetComponent<MapCubeManager>();
+                TileColor _color;
+                _color = TileColor.red;
+                _mapManager.changeAllTileColor(_color);
+                _mapManager.changeAllTileType(MapCubeManager.TileType.Changeable);
+
+            }
+        }
         StartCoroutine(TransformSeparately());
         transformed = true;
     }
 
-    IEnumerator TransformSeparately() {
+    IEnumerator TransformSeparately()
+    {
         // if (endPositions == null || endPositions.Length != transform.childCount)
         //     yield return null;
         for (int i = 0; i < endPositions.Length - 1; i++)
         {
-           Debug.Log("start transforming " + i);
-           yield return StartCoroutine(Transforming(i));
+            Debug.Log("start transforming " + i);
+            yield return StartCoroutine(Transforming(i));
         }
         YTransform yTransform = transform.GetComponentInChildren<YTransform>();
-        yTransform.Begin();        
+        yTransform.Begin();
         yield return new WaitForSeconds(5f);
 
         SceneManager.LoadScene("Credit");
 
     }
 
-    IEnumerator Transforming(int childIndex) {
+    IEnumerator Transforming(int childIndex)
+    {
         Transform child = transform.GetChild(childIndex);
-        while (elapsedTime < desiredDuration) {
+        while (elapsedTime < desiredDuration)
+        {
             // Debug.Log(elapsedTime);
             elapsedTime += Time.deltaTime;
             float percentageComplete = elapsedTime / desiredDuration;
             child.localPosition = Vector3.Lerp(startPositions[childIndex], endPositions[childIndex], percentageComplete);
             child.localRotation = Quaternion.Lerp(startRotations[childIndex], endRotations[childIndex], percentageComplete);
-            child.localScale = Vector3.Lerp(initialSizes[childIndex], scales[childIndex] * initialSizes[childIndex], percentageComplete );
+            child.localScale = Vector3.Lerp(initialSizes[childIndex], scales[childIndex] * initialSizes[childIndex], percentageComplete);
             yield return null;
         }
         elapsedTime = 0;
